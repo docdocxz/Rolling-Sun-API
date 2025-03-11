60227f55ec968fe3188928a9011efd08fd598025
docdocxz <juliandiniro@gmail.com>
2025-03-11 11:51:51 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothINC_API.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class ClothINC_APIController : ControllerBase {
+
+        }
+    }
