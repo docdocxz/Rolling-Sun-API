9cb29b8f9efa23587e303127cb48267157601cfb
docdocxz <juliandiniro@gmail.com>
2025-03-13 22:39:37 -0300

    Hechos modelos de cortinas

---
+++ Interfaces/ITela.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using RollingSun.Models.Telas;
+
+namespace RollingSun.Interfaces {
+    public interface ITela {
+        float Gramaje { get; }
+        bool IsSemitransparent { get; }
+        Color Tinte { get; set; }
+        }
+    }
